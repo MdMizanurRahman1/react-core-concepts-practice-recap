1ec5fdcdb49250a6ce95785b49d4a835379004fa
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-03-23 11:45:36 +0200

    react posts prcticed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District></District>
+      <LoadPosts></LoadPosts>
     </div>
   );
 }
+
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+
+  return (
+    <div className='container'>
+      <h2>Posts: {posts.length} </h2>
+      <div className="row row-cols-3">
+        {
+          posts.map(post => <Post
+            post={post}
+            key={post.id}
+          ></Post>)
+        }
+      </div>
+    </div>
+
+  )
+}
+
+function Post(props) {
+  const { title, body } = props.post
+  return (
+    <div >
+      <div className='m-2 py-5 px-2 bg-body-secondary h-75'>
+        <h3 className='fs-6'>Title: {title}</h3>
+        <p className='fst-normal'>Body: {body}</p>
+      </div>
+    </div>
+  )
+}
+
+
+
+function District() {
+  const [power, setPower] = useState(1);
+  const boostPower = () => {
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+  return (
+    <div>
+      <h4 className='m-4'>Power: {power}</h4>
+      <button className="btn btn-warning" onClick={boostPower}>Boost the Power</button>
+    </div >
+  )
+}
+
 
 export default App;
